175a84ef3a44539f50a712be5562178b19cfb522
Ester Zhao <ester@Esters-MacBook-Pro.local>
2019-11-17 17:09:27 -0500

    Populate object classes

--- src/PaymentSys.java
+++ src/PaymentSys.java
@@ -1,4 +1,23 @@
+import java.util.Random;
 
 public class PaymentSys {
-
+	
+	/**
+	 * Payment System class constructor
+	 * 
+	 * TODO: More specific methods? Decide how to use this
+	 */
+	public PaymentSys() {
+		
+	}
+	
+	public boolean validate(int creditNum, int CVV, String expirationDate) {
+		Random rand = new Random();
+		int i = rand.nextInt(10);
+		if (i != 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
