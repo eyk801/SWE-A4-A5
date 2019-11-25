1f3bd3acf538d8a8b53f296d556339fc6390368c
Ester Zhao <ester@Esters-MacBook-Pro.local>
2019-11-25 16:26:45 -0500

    Payment system javadoc updates

--- src/PaymentSys.java
+++ src/PaymentSys.java
@@ -1,16 +1,26 @@
 import java.util.Random;
 
+/**
+ * @author      Ali Eshghi, Ester Zhao
+ * @version     1.0
+ */
 public class PaymentSys {
 	
 	/**
-	 * Payment System class constructor
-	 * 
-	 * TODO: More specific methods? Decide how to use this
+	 * Payment System constructor.
 	 */
 	public PaymentSys() {
 		
 	}
 	
+	/**
+	 * Validate input credit card information. Validates 90% of requests.
+	 * </p>
+	 * @param creditNum			credit card number
+	 * @param CVV				credit card CVV
+	 * @param expirationDate	credit card expiration date
+	 * @return
+	 */
 	public boolean validate(long creditNum, int CVV, String expirationDate) {
 		Random rand = new Random();
 		int i = rand.nextInt(10);
