323d174244e37919114ec0a14477b86acdbf2beb
Ester Zhao <ester@Esters-MacBook-Pro.local>
2019-11-25 15:54:50 -0500

    User javadoc updates

--- src/User.java
+++ src/User.java
@@ -1,21 +1,39 @@
 import java.util.List;
 import java.util.ArrayList;
 
+/**
+ * @author      Ali Eshghi, Charlotte Gephart, Emily Kim, Ester Zhao
+ * @version     1.0
+ */
 public class User {
-	private String username = "";
-	private String password = "";
+	/** User unique username */
+	private String username = new String();
+	/** User password */
+	private String password = new String();
+	/** Membership type (0-2) */
 	private int membershipType = 0;
+	/** User credit card number */
 	private long creditCardNum = 0;
+	/** User credit card CVV number */
 	private int CVV = 0;
-	private String expirationDate = "";
+	/** User credit card expiration date */
+	private String expirationDate = new String();
+	/** List of user's past ride ids */
 	private List<Integer> rideHistory = new ArrayList<>();
+	/** Ride id number for user current ride. Default to 0.*/
 	private int currentRideId = 0;
+	/** User total bill */
 	private int totalBill = 0;
 	
 	/**
-	 * User class constructor
-	 * 
-	 * TODO: Include any methods involving payment system, check toString methods
+	 * User class constructor.
+	 * </p>
+	 * @param username
+	 * @param password
+	 * @param membershipType
+	 * @param creditCardNum
+	 * @param CVV
+	 * @param expirationDate
 	 */
 	public User(String username, String password, int membershipType, 
 			long creditCardNum, int CVV, String expirationDate) {
@@ -27,53 +45,127 @@ public class User {
 		this.expirationDate = expirationDate;
 	}
 	
-
 	/**
-	 * This is where we will check that user is valid (re: payment methods)
-	 * @return
-	 * TODO: implement
+	 * Add ride to user history.
+	 * </p>
+	 * @param rideID	the id of user's ride
 	 */
-	public boolean validUser() {
-		return true;
-	}
-	
 	public void addUserRide(int rideID) {
 		rideHistory.add(rideID);
 	}
 	
+	/**
+	 * Getter for user id.
+	 * </p>
+	 * @return username	
+	 */
 	public String getId() {
 		return this.username;	
 	}
 	
-	public int getCurrentRideId() {
-		return this.currentRideId;
-	}
-	
-	public void setCurrentRideId(int currRideId) {
-		this.currentRideId = currRideId;
-	}
-	
+	/**
+	 * Setter for username.
+	 * @param newId			new username
+	 */
 	public void setId(String newId) {
 		this.username = newId;
 	}
 	
-	public void setPassword(String newPass) {
-		this.password = newPass;
+	/**
+	 * Getter for current ride id.
+	 * </p>
+	 * @return currentRideId
+	 */
+	public int getCurrentRideId() {
+		return this.currentRideId;
 	}
 	
+	/**
+	 * Setter for current ride id.
+	 * </p>
+	 * @param currRideId	new user ride id
+	 */
+	public void setCurrentRideId(int currRideId) {
+		this.currentRideId = currRideId;
+	}
+	
+	/**
+	 * Getter for user password.
+	 * </p>
+	 * @return password
+	 */
 	public String getPassword() {
 		return this.password;
 	}
 	
+	/**
+	 * Setter for user password
+	 * </p>
+	 * @param newPass		new user password
+	 */
+	public void setPassword(String newPass) {
+		this.password = newPass;
+	}
+	
+	/**
+	 * Getter for user membership type
+	 * </p>
+	 * @return membershipyType
+	 */
 	public int getType() {
 		return this.membershipType;
 	}
 	
-	
+	/**
+	 * Setter for user membershipy type
+	 * </p>
+	 * @param newMembershipType		new membership type
+	 */
 	public void setType(int newMembershipType) {
 		this.membershipType = newMembershipType;
 	}
 	
+	/**
+	 * Getter for user ride history
+	 * </p>
+	 * @return rideHistory		List of user's previous ride ids
+	 */
+	public List<Integer> getRides() {
+		return this.rideHistory;
+	}
+	
+	/**
+	 * Setter for user ride history
+	 * </p>
+	 * @param rides		List of user's prev ride ids
+	 */
+	public void setRides(List<Integer> rides) {
+		this.rideHistory = rides;
+	}
+	
+	/**
+	 * Getter for user accumulated bill
+	 * </p>
+	 * @return totalBill
+	 */
+	public Integer getBill() {
+		return this.totalBill;
+	}
+	
+	/**
+	 * Add to user bill
+	 * </p>
+	 * @param bill		Amount to add to the user total bill
+	 */
+	public void addToBill(Integer bill) {
+		this.totalBill += bill;
+	}
+	
+	/**
+	 * End a user ride.
+	 * </p>
+	 * Update ride history and reset currentRideId to 0.
+	 */
 	public void endRide() {
 		// Add ride to user history
 		addUserRide(this.currentRideId);
@@ -81,6 +173,11 @@ public class User {
 		this.currentRideId = 0;
 	}
 	
+	/**
+	 * Checks whether a user is currently on a ride.
+	 * </p>
+	 * @return boolean	false if the currentRideId is 0, and true otherwise
+	 */
 	public boolean onRide() {
 		if (this.currentRideId == 0) {
 			return false;
@@ -89,22 +186,11 @@ public class User {
 		}
 	}
 	
-	public List<Integer> getRides() {
-		return this.rideHistory;
-	}
-	
-	public void setRides(List<Integer> rides) {
-		this.rideHistory = rides;
-	}
-	
-	public Integer getBill() {
-		return this.totalBill;
-	}
-	
-	public void addToBill(Integer bill) {
-		this.totalBill += bill;
-	}
-	
+	/**
+	 * Converts user ride history to a csv style string.
+	 * </p>
+	 * @return rideIds	String in csv style of user ride history ids
+	 */
 	public String ridesToString() {
     	// Make List rideHistory into a csv line
     	String rideIds = "";
@@ -123,6 +209,11 @@ public class User {
     	return rideIds;
 	}
 	
+    /**
+     * toString method for data to write to csv data files
+     * </p>
+     * @return toString	the string to store in the user-data.csv
+     */
     public String toSaveString() {
     	String rideIds = this.ridesToString();
         return (this.username + "," + this.password + "," + this.membershipType + "," + 
@@ -132,6 +223,11 @@ public class User {
         		rideIds + "\n");
     }
     
+    /**
+     * toString method for user data to output onto console
+     * </p>
+     * @return toString	the string to print to the console
+     */
     public String toViewString(){
     	String rideIds = this.ridesToString();
     	return (" " + this.username + "\t" 
