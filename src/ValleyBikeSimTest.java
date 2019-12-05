5c3ce54fd259edfc32ca235b418eb5b6e8daf8de
Ali Eshghi <leshghi@smith.edu>
2019-12-05 17:34:42 -0500

    more tests

--- src/ValleyBikeSimTest.java
+++ src/ValleyBikeSimTest.java
@@ -38,7 +38,7 @@ class ValleyBikeSimTest {
 
 	@Test
 	void testReadData_validData() {
-		assertEquals(vBSTester.getBikes().size(), 1);
+		assertEquals(vBSTester.getBikes().size(), 3);
 		assertEquals(vBSTester.getStations().size(), 2);
 		assertEquals(vBSTester.getRides().size(), 3);
 		assertEquals(vBSTester.getCurrRides().size(), 0);
@@ -50,10 +50,23 @@ class ValleyBikeSimTest {
 	@Test
 	void testCheckOutBike_rideCreated_dataUpdated() {
 		String user1 = "emilykim";
-		assertEquals(vBSTester.getStations().get(1).getNumBikes(), 1);
+		assertEquals(vBSTester.getStations().get(1).getNumBikes(), 3);
 		vBSTester.checkOutBike(user1, 1);
-		assertEquals(vBSTester.getStations().get(1).getNumBikes(), 0);
+		assertEquals(vBSTester.getStations().get(1).getNumBikes(), 2);
 		assertEquals(vBSTester.getCurrRides().size(), 1);
 		assertEquals(vBSTester.getRides().size(), 4);
+		vBSTester.checkInBike(user1, 1);
+	}
+	
+	@Test
+	void testCheckInBike_rideCreated_dataUpdated() {
+		int ridesSize = vBSTester.getRides().size();
+		String user2 = "esterzhao";
+		vBSTester.checkOutBike(user2, 1);
+		assertEquals(vBSTester.getStations().get(1).getNumBikes(), 2);
+		vBSTester.checkInBike(user2, 1);
+		assertEquals(vBSTester.getStations().get(1).getNumBikes(), 3);
+		assertEquals(vBSTester.getCurrRides().size(), 0);
+		assertEquals(vBSTester.getRides().size(), ridesSize+1);
 	}
 }
