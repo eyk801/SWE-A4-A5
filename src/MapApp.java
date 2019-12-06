034817c725b67bf8475f39e7a70a605127c48dc1
Charlotte Gephart <charlottegephart@Charlottes-MacBook-Pro.local>
2019-12-06 17:49:13 -0500

    dots now work, errors remain

--- src/MapApp.java
+++ src/MapApp.java
@@ -8,143 +8,101 @@ import java.util.*;
 import javax.imageio.ImageIO;
 import java.awt.*; 
 
-public class MapApp extends JPanel {
+public class MapApp{
 	
-	private ArrayList<Point> points  = new ArrayList<Point>();
-	private JFrame dialog;
-	private boolean user;
-	BufferedImage mapImage;
-	JFrame frame;
-	
-	public void MapApp() {
-
+	public MapApp() {
+		SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new JFrame();
+                frame.add(new App());
+                frame.pack();
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+            }
+        });		
 	}
-	
-	/**
-     *  Creates and shows the GUI.
-     */
-    public void createGUI() {
 
-    	frame = new JFrame();
+    
+    public class App extends JPanel {
     	
-    	JPanel panel = new JPanel();
-    	panel.setLayout(new FlowLayout());
+    	private ArrayList<Point> points  = new ArrayList<Point>();
+    	private JFrame dialog;
+    	private boolean user = false;
+    	BufferedImage mapImage;
+    	JFrame frame;
     	
-    	int imageWidth;
-    	int imageHeight;
-		try {
-			mapImage = ImageIO.read(new File("data-files/ValleyBikeMap.png"));
-			JLabel picLabel = new JLabel(new ImageIcon(mapImage));
-			imageWidth = mapImage.getWidth();
-			imageHeight = mapImage.getHeight();
-			frame.setSize(imageWidth, imageHeight);
-	    	panel.add(picLabel);
-		} catch (IOException e) {
-			System.out.println("Invalid file name.");
-			e.printStackTrace();
-		}
-		
-		frame.addMouseListener(new MouseListener());
-		frame.addMouseMotionListener(new MouseListener());
-		
-		frame.add(panel);		
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+    	public App() {
+    		try {
+    			mapImage = ImageIO.read(new File("data-files/ValleyBikeMap.png"));
+    		} catch (IOException e) {
+    			System.out.println("Invalid file name.");
+    			e.printStackTrace();
+    		}
+    		
+    		addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                	Point p = new Point(e.getX(), e.getY());
+    				System.out.println("Click event at (" + p + ")");
+    				if (user == true) {
+    					//get station from coordinates here
+    					showInfo();
+    				}
+    				else {
+    					if (confirmStation() == 0) {
+    						points.add(p);
+    			            System.out.println(points);
+    			            
+    			            if (mapImage != null) {
+    			            	System.out.println("circle");
+    		                    Graphics2D g2d = mapImage.createGraphics();
+    		                    g2d.setColor(Color.black);
+    		                    g2d.fillOval(p.x - 10, p.y - 10, 20, 20);
+    		                    g2d.dispose();
+    		                    repaint();
+    		                }
+    			            
+    			            frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+    					}           
+    				}
+                }
+            });
+    		
+    	}
+    	
+    	@Override
+        public Dimension getPreferredSize() {
+            return mapImage == null ? new Dimension(200, 200) : new Dimension(mapImage.getWidth(), mapImage.getHeight());
+        }
+    	
+        @Override
+        protected void paintComponent(Graphics g) {
+        	super.paintComponent(g);
+            Graphics2D g2d = (Graphics2D) g.create();
+            g2d.drawImage(mapImage, 0, 0, this);
+            g2d.dispose();
+        }
+        
+        public void showInfo() {
+        	JOptionPane.showMessageDialog(dialog,
+        			"Info",
+            		"Station Information",
+            		JOptionPane.INFORMATION_MESSAGE);
+        }
+        
+        public int confirmStation() {
+        	return JOptionPane.showConfirmDialog(dialog,
+        			"Is this location correct?",
+        			"Confirm Station",       		
+            	    JOptionPane.YES_NO_OPTION);
+        }
+        
+    	boolean inBounds(int mouseX, int mouseY, int x, int y) {
+    	    return ((mouseX >= x - 10) && (mouseY >= y - 10) && (mouseX < x + 10) && (mouseY < y + 10));
+    	}
+    	
     }
-    
-
-    @Override
-    protected void paintComponent(Graphics g) {
-    	super.paintComponent(g);
-        Graphics2D g2d = (Graphics2D) g.create();
-        g2d.drawImage(mapImage, 0, 0, this);
-        g2d.dispose();
-    }
-    
-    public void showInfo() {
-    	JOptionPane.showMessageDialog(dialog,
-    			"Info",
-        		"Station Information",
-        		JOptionPane.INFORMATION_MESSAGE);
-    }
-    
-    public int confirmStation() {
-    	return JOptionPane.showConfirmDialog(dialog,
-    			"Is this location correct?",
-    			"Confirm Station",       		
-        	    JOptionPane.YES_NO_OPTION);
-    }
-    
-	boolean inBounds(int mouseX, int mouseY, int x, int y) {
-	    return ((mouseX >= x - 10) && (mouseY >= y - 10) && (mouseX < x + 10) && (mouseY < y + 10));
-	}
-	
-	
-	
-
-	
-	public static void runUserMap() {
-		final MapApp userMap = new MapApp();
-		userMap.user = true;
-		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-			    userMap.createGUI();
-			}
-		});
-	}
-	
-
-	public static void runBackendMap() {
-		final MapApp backendMap = new MapApp();
-		backendMap.user = false;
-		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-			    backendMap.createGUI();
-			}
-		});
-	}
-	
-
-	private class MouseListener extends MouseAdapter {
-		@Override
-		public void mouseClicked(MouseEvent e) {
-			Point p = new Point(e.getX(), e.getY());
-			System.out.println("Click event at (" + p + ")");
-			if (user == false) {
-				showInfo();
-			}
-			else {
-				if (confirmStation() == 0) {
-					points.add(p);
-		            System.out.println(points);
-		            
-		            if (mapImage != null) {
-		            	System.out.println("circle");
-	                    Graphics2D g2d = mapImage.createGraphics();
-	                    g2d.setColor(Color.black);
-	                    g2d.fillOval(p.x - 10, p.y - 10, 20, 20);
-	                    g2d.dispose();
-	                    repaint();
-	                }
-		            
-		            frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
-				}           
-			}
-		}
-		public void mousePressed(MouseEvent e) {
-
-		}
-		public void mouseReleased(MouseEvent e) {
-
-		}
-		public void mouseEntered(MouseEvent e) {
-
-		}
-		public void mouseExited(MouseEvent e) {
-
-		}
-    }
-
 }
 
 
