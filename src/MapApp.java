e4e1727833f3f41e7cd3ec41014ebdc8bc51d9c6
Charlotte Gephart <charlottegephart@Charlottes-MacBook-Pro.local>
2019-12-05 20:12:04 -0500

    Base MapApp

--- src/MapApp.java
+++ src/MapApp.java
@@ -4,52 +4,65 @@ import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.*;
+
 import javax.imageio.ImageIO;
 
-public class MapApp {
+public class MapApp extends JPanel {
 	
-	private AppStationVisual station;
+	private ArrayList<Point> points;
 	
 	public void MapApp() {
-		
-		station = new AppStationVisual(500,500);
+		points = new ArrayList<Point>();
 	}
 	
 	/**
      *  Creates and shows the GUI.
      */
     public void createGUI() {
-		// Create and set up the window.
-		JFrame frame = new JFrame("ValleyBike Station Map");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	
-		// Add map image
-		createMap(frame.getContentPane());
-	
-		// Display the window.
-		frame.pack();
-		frame.setVisible(true);
-    }
-	
-    public void createMap(Container pane) {
-    	pane.setLayout(new FlowLayout());
+    	
+    	JFrame frame = new JFrame();
+    	
     	JPanel panel = new JPanel();
     	panel.setLayout(new FlowLayout());
     	
     	BufferedImage mapImage;
+    	int imageWidth;
+    	int imageHeight;
 		try {
 			mapImage = ImageIO.read(new File("data-files/ValleyBikeMap.png"));
 			JLabel picLabel = new JLabel(new ImageIcon(mapImage));
+			imageWidth = mapImage.getWidth();
+			imageHeight = mapImage.getHeight();
+			frame.setSize(imageWidth, imageHeight);
 	    	panel.add(picLabel);
 		} catch (IOException e) {
 			System.out.println("Invalid file name.");
 			e.printStackTrace();
 		}
-    	pane.add(panel);
-    	
-    	pane.addMouseListener(new MouseListener());
-		pane.addMouseMotionListener(new MouseListener());
+
+		addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                points.add(new Point(e.getX(), e.getY()));
+                repaint();
+            }
+        });
 		
+		frame.add(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);     
+        frame.setVisible(true);
+    }
+    
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.black);
+        for (Point point : points) {
+            g2.fillOval(point.x, point.y, 20, 20);
+        }
     }
 
 	
@@ -72,29 +85,6 @@ public class MapApp {
 		});
 	}
 	*/
-	
-    
-	
-	private class MouseListener extends MouseAdapter {
-		public void mouseClicked(MouseEvent e) {
-			//e.getX();
-			//e.getY();
-			System.out.println("Click event at (" + e.getX() + "," + e.getY() + ")");
-	
-		}
-		public void mousePressed(MouseEvent e) {
-	
-		}
-		public void mouseReleased(MouseEvent e) {
-	
-		}
-		public void mouseEntered(MouseEvent e) {
-	
-		}
-		public void mouseExited(MouseEvent e) {
-	
-		}
-    }
 
 }
 
