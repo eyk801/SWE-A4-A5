650a12c08cd91aeb6de82e7bf95c84038e9553f7
Ester Zhao <esterzhao@gmail.com>
2019-12-09 14:52:09 -0500

    Clean up MapApp

--- src/MapApp.java
+++ src/MapApp.java
@@ -1,13 +1,15 @@
+/**
+ * @author      Charlotte Gephart, Ester Zhao
+ * @version     1.0
+ */
 import javax.swing.*; 
-import java.awt.*; 
+import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.text.ParseException;
 import java.util.*;
 import javax.imageio.ImageIO;
-import java.awt.*; 
 
 public class MapApp{
 	/** Instance of the ValleyBikeSim */
